1cf77a96fc5ed82f1684e02b704e67b8de1a94a3
Simone <>
2025-06-17 21:05:52 +0200

    46

--- src/index.js
+++ src/index.js
@@ -894,8 +894,11 @@ window.addEventListener("message", function(event) {
   }
 
   if (event.data && event.data.type === "showCursor") {
-    customCursor.style.transform = `translate(${cursorX}px, ${cursorY}px) translate(-50%, -50%) scale(1)`;
-    customCursor.style.opacity = "1";
+  // Aggiorna subito le coordinate del cursore per evitare trascinamento
+  cursorX = targetX;
+  cursorY = targetY;
+  customCursor.style.transform = `translate(${cursorX}px, ${cursorY}px) translate(-50%, -50%) scale(1)`;
+  customCursor.style.opacity = "1";
   }
 });
 
